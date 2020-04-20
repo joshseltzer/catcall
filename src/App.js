147ad486b3b912270b2ae8b27c213c3014ae3b45
Josh Seltzer <jyseltz@gmail.com>
2020-04-19 20:27:42 -0400

    CRA barebones

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="video-box">
+        <div class="static-motion"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
